6e0fe124e9100a217468cfde7de684e0f7001812
takahiro38k <grandeur38k@gmail.com>
2020-04-20 11:29:37 +0900

    implement getting started after webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+import _ from 'lodash'
+
 const component = () => {
   const element = document.createElement('div')
-  const array = ['Hello', 'webpack']
+  const array = ['Hello', 'webpack!!', 'From JS']
   // CDNから利用するlodashは、最初からアンスコで利用できる。
   // array配列を半角スペースで連結。
   element.innerHTML = _.join(array, ' ')
