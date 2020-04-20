066614057cbb1ca5e43d64292069fabb995ffaf5
takahiro38k <grandeur38k@gmail.com>
2020-04-20 16:04:42 +0900

    implement some modules

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 import _ from 'lodash'
 
+/*
+import { NiJou, NAME } from './utilities'
+console.log(NiJou(9))
+console.log(NAME)
+ */
+
+/*
+import * as utilities from './utilities'
+console.log(utilities.NiJou(5))
+console.log(utilities.NAME)
+ */
+
+/*
+import Lion, { NAME as NAME_OF_HIM } from './utilities'
+console.log(NAME_OF_HIM)
+console.log(Lion.say())
+ */
+
+// default のimport名は自由に設定できる。
+import Tiger from './utilities'
+console.log(Tiger.say())
+
 const component = () => {
   const element = document.createElement('div')
   const array = ['Hello', 'webpack!!', 'From JS']
